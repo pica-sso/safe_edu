f87906119cf2d224b784acd42923f195416ba6df
seyeong.so <seyeong.so@amkor.co.kr>
2025-02-04 11:55:44 +0900

    remove not using functions

--- main.py
+++ main.py
@@ -14,33 +14,8 @@ class SafeEdu:
         self.service = Service(
             executable_path=r'C:\source\seyeong_draft\chromedriver-win64\chromedriver.exe')  # ChromeDriver 경로 지정
         chrome_options = Options()
-        chrome_options.set_capability('loggingPrefs', {'performance': 'ALL'})
         self.driver = webdriver.Chrome(service=self.service, options=chrome_options)
-
-        self.dev_tools, _ = self.driver.start_devtools()
-        self.enable_network_monitoring()
         self.open_site()
-        self.driver.execute_cdp_cmd("Performance.enable", {})
-
-    def enable_network_monitoring(self):
-        self.driver.execute_cdp_cmd('Network.enable', {})
-        self.driver.request_interceptor = self.on_response
-
-    def on_request(self, request):
-        """요청이 보내질 때마다 호출되는 콜백 함수"""
-        print(f"요청 URL: {request['url']}")
-
-    def on_response(self, response):
-        """네트워크 응답 처리"""
-        if 'insert' in response['response']['url']:  # 응답 URL에 'insert' 포함 여부 확인
-            try:
-                # JSON 응답 데이터 출력
-                data = response['response']
-                if 'body' in data:
-                    json_data = json.loads(data['body'])
-                    print(f"Insert 데이터를 찾았습니다: {json_data}")
-            except Exception as e:
-                print(f"응답 처리 중 오류 발생: {e}")
 
     def open_site(self):
         self.driver.get('https://corp.edukisa.or.kr/home.do#')
@@ -85,24 +60,9 @@ class SafeEdu:
                         self.click('//*[@id="myclass-edu-attn-window"]/div[1]/label/span')
                         self.click('//*[@id="myclass-edu-attn-window"]/div[2]/div/button[2]')
                         time.sleep(0.5)
-                        self.log_network_traffic()
+                        # self.log_network_traffic()
         except Exception as e:
             print(e)
-
-    def log_network_traffic(self):
-        """네트워크 요청 및 응답 로그를 출력"""
-        # 성능 로그 (performance) 가져오기
-        logs = self.driver.get_log("performance")
-        for entry in logs:
-            message = json.loads(entry['message'])
-            if 'message' in message and 'Network.responseReceived' in message['message']['method']:
-                response = message['message']['params']['response']
-                if 'insert' in response.get('url', ''):  # 특정 URL에 대한 응답 처리
-                    print("Insert 데이터 찾음:")
-                    print(json.dumps(response, indent=2))
-
-    def get_network_answer(self):
-        pass
 
     def wait_for_progress_and_click(self):
         try:
